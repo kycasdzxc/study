e658e8010a21de41f144f5c62dd16995ccebed21
Yermi <105793579+kycasdzxc@users.noreply.github.com>
2022-12-23 18:35:03 +0900

    CRUD 작업 중

--- ProductManager/src/service/ProductService.java
+++ ProductManager/src/service/ProductService.java
@@ -44,34 +44,66 @@ public class ProductService {
 	
 	// 상품 추가
 	public void register() {
+		System.out.print("상품 ID를 입력해주세요. > ");
 		String id = sc.nextLine();
 		
 		if(findBy(id) == null) {
+			System.out.print("상품명을 입력해주세요. > ");
+			String name = sc.nextLine();
+			
+			System.out.print("가격을 입력해주세요. > ");
+			int price = Integer.parseInt(sc.nextLine());
+			
+			System.out.print("상품정보를 입력해주세요. > ");
+			String detail = sc.nextLine();
+			
+			System.out.print("재고를 입력해주세요. > ");
+			int stock = Integer.parseInt(sc.nextLine());;
+			
+			// Product에 값 세팅
 			Product product = new Product();
 			product.setId(id);
+			product.setName(name);
+			product.setPrice(price);
+			product.setDetail(detail);
+			product.setStock(stock);
 			
-			String name = sc.nextLine();
-			int price;
-			String detail;
-			int stock;
-			
-//			products.add(product);
+			products.add(product);
 			save();
 		} else {
-			System.out.println("이미 존재하는 상품입니다.");
+			System.out.println("존재하는 상품 ID입니다.");
 		}
 	}
 	
 	// 상품 수정
 	public void modify() {
+		System.out.print("수정할 상품의 ID를 입력해주세요. > ");
+		String id = sc.nextLine();
+		
+		Product product = findBy(id);
+		
+		System.out.println("수정할 항목을 선택해주세요.");
+		System.out.print("1)상품명 2)가격 3)상품정보 4)재고 > ");
+		int num = Integer.parseInt(sc.nextLine());
 		
 		save();
 	}
 	
 	// 상품 삭제
 	public void delete() {
+		System.out.print("삭제할 상품의 ID를 입력해주세요. > ");
+		String id = sc.nextLine();
 		
-		save();
+		int index = findByIndex(id);
+		
+		if(index != -1) {
+			Product product = products.get(index);
+			products.remove(index);
+			System.out.println("해당 상품이 삭제되었습니다. [ " + product.getName() + " ] ");
+			save();
+		} else {
+			System.out.println("상품 ID가 존재하지 않습니다.");
+		}
 	}
 	
 	// 상품 검색
@@ -93,6 +125,7 @@ public class ProductService {
 		return word.toLowerCase().trim().replace(" ", "");
 	}
 	
+	// ID로 상품 조회
 	private Product findBy(String id) {
 		Product product = null;
 		
@@ -103,6 +136,19 @@ public class ProductService {
 			}
 		}
 		return product;
+	}
+	
+	// ID로 상품 index 조회
+	private int findByIndex(String id) {
+		int index = -1;
+		
+		for(int i = 0 ; i < products.size() ; i++) {
+			if(id.equals(products.get(i).getId())) {
+				index = i;
+				break;
+			}
+		}
+		return index;
 	}
 	
 	// 데이터 저장
